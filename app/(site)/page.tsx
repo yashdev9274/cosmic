f903d6d9fee20ddd44128031821c4a016f677077
yashdev9274 <yashdev.yvd@gmail.com>
2024-01-06 12:19:52 +0530

    Add new page, sidebar and box components

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='text-green-500'>Hello Cosmic</div>
+    <div className='text-green-500'>Hello (Site) !</div>
   )
 }
---
+++ components/Sidebar.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+
+
+
+import React from "react";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import Box from "./Box";
+
+interface SidebarProps {
+    children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({
+    children
+}) => {
+
+
+    const pathname = usePathname();
+
+    const routes = useMemo(() => [
+        {
+            icon: HiHome,
+            label: 'Home',
+            active: pathname!== '/search',
+            href: '/'
+        },
+
+        {
+            icon: BiSearch,
+            label: 'Search',
+            href: '/search',
+            active: pathname === '/search',
+        }
+    ], [pathname]);
+
+    return (
+        <div className = "flex h-full">
+            <div
+                className="
+                    hidden
+                    md:flex
+                    flex-col
+                    gap-y-2
+                    bg-black
+                    h-full
+                    w-[300px]
+                    p-2
+                "
+            >
+                <Box>
+                    Sidebar Navigation
+                </Box>
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
