e3f9cda45aa57556a5de4e426465b85cb74e9443
yashdev9274 <yashdev.yvd@gmail.com>
2024-01-12 09:13:15 +0530

    error fixed SupabaseProvider

--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,31 +1,27 @@
 "use client";
 
-import { Database } from "@/types_db";
-import { SupabaseClient, createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+import { Database } from "@/types_db";
 
 interface SupabaseProviderProps {
-    children: React.ReactNode;
-}
-
-
-
+  children: React.ReactNode;
+};
 
 const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
-    children,
+  children
 }) => {
+    const [supabaseClient] = useState(() =>
+    createClientComponentClient<Database>()
+  );
 
-    const SupabaseProvider = useState(()=>
-        createClientComponentClient<Database>()
-    );
-
-    return ( 
-        <SessionContextProvider supabaseClient={supabaseClient}>
-            {children}
-        </SessionContextProvider>
-
-     );
+  return ( 
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
 }
  
 export default SupabaseProvider;
