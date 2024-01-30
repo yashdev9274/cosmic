4b08eed0cce2ac3402dc751b914e60609d36b740
yashdev9274 <yashdev.yvd@gmail.com>
2024-01-30 13:58:02 +0530

    Add useUploadModal hook and implement onChange function in UploadModal

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,8 +1,26 @@
+"use client";
+
+import useUploadModal from "@/hooks/useUploadModal";
 import Modal from "./Modal";
 
 const UploadModal = () => {
+
+    const uploadModal = useUploadModal();
+    const onChange = (open: boolean)=>{
+        if(!open){
+            // 
+
+            uploadModal.onClose();
+        }
+    }
     return ( 
-        <Modal>
+        <Modal
+            title="Upload modal title"
+            description="Upload modal description"
+            isOpen = {uploadModal.isOpen}
+            onChange={onChange}
+        >
+            Upload Content
         </Modal>
      );
 }
