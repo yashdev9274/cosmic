693d877e23275ea0fd613f0fbb175b6fbb856a6f
yashdev9274 <yashdev.yvd@gmail.com>
2024-02-02 12:11:43 +0530

    Update form inputs and submit button to UploadModal component

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -3,6 +3,7 @@ import useUploadModal from "@/hooks/useUploadModal";
 import Modal from "./Modal";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Input from "./Input";
+import Button from "./Button";
 
 const UploadModal = () => {
     const [isLoading, setIsLoading] = useState(false);
@@ -37,13 +38,54 @@ const UploadModal = () => {
             isOpen={uploadModal.isOpen}
             onChange={onChange}
         >
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form 
+                onSubmit={handleSubmit(onSubmit)}
+                className="flex flex-col gap-y-4"
+            >
                 <Input
                     id="title"
                     disabled={isLoading}
                     {...register('title', { required: true })}
-                    placeholder="Song Title"
+                    placeholder="Song title"
                 />
+
+                <Input
+                    id="author"
+                    disabled={isLoading}
+                    {...register('author', { required: true })}
+                    placeholder="Song author"
+                />
+
+                <div>
+                    <div className="pb-1">
+                        Select a song file
+                    </div>
+                    <Input
+                        placeholder="test"
+                        disabled={isLoading}
+                        type="file"
+                        accept=".mp3"
+                        id="song"
+                        {...register('song', { required: true })}
+                    />
+                </div>
+
+                <div>
+                    <div className="pb-1">
+                        Select an image song cover
+                    </div>
+                    <Input
+                        placeholder="test"
+                        disabled={isLoading}
+                        type="file"
+                        accept="image/*"
+                        id="image"
+                        {...register('image', { required: true })}
+                    />
+                </div>
+                <Button disabled={isLoading} type="submit">
+                    Create
+                </Button>
             </form>
         </Modal>
     );
